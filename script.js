a245132e48727f607a03ccbfc5d9d6d3ae5c5b95
ameersalama <78989052+ameersalama@users.noreply.github.com>
2021-03-23 19:34:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var list = [
+  {
+    name: "Harry",
+    age: 25
+  },
+  {
+    name: "Larry",
+    age: 15
+  },
+  {
+    name: "Bert",
+    age: 45
+  },
+  {
+    name: "Jerry",
+    age: 35
+  },
+  {
+    name: "Berry",
+    age: 5
+  },
+  {
+    name: "Bart",
+    age: 55
+  }
+];
